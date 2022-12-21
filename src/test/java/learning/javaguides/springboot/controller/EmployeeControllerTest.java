007995bf4dc6434a819e14e9d1c426d31a974ac8
Devang Chauhan <chauhan.devam@gmail.com>
2022-12-21 09:46:31 +0530

    SBT-12: implemented a junit testcases for update and delete employee methods

--- src/test/java/learning/javaguides/springboot/controller/EmployeeControllerTest.java
+++ src/test/java/learning/javaguides/springboot/controller/EmployeeControllerTest.java
@@ -13,12 +13,13 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -90,6 +91,132 @@ public class EmployeeControllerTest {
         response.andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("$.size()", is(employees.size())));
+    }
+
+    //positive scenario - valid employee id
+    //JUnit Test for GET employee by id REST API
+    @Test
+    public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        Employee employee = Employee.builder()
+                .firstName("Devang")
+                .lastName("Chauhan")
+                .email("devang@gmail.com")
+                .build();
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(employee));
+
+        //when - action or behaviour we are going to test
+        ResultActions response = mockMvc.perform(get("/api/employees/{id}", employeeId));
+
+        //then - verify the resul or output using assert statements
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail()
+                )));
+    }
+
+    //negative scenario - valid employee id
+    //JUnit Test for GET employee by id REST API
+    @Test
+    public void givenInvalidEmployeeId_whenGetEmployeeById_thenReturnEmpty() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        Employee employee = Employee.builder()
+                .firstName("Devang")
+                .lastName("Chauhan")
+                .email("devang@gmail.com")
+                .build();
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+
+        //when - action or behaviour we are going to test
+        ResultActions response = mockMvc.perform(get("/api/employees/{id}", employeeId));
+
+        //then - verify the resul or output using assert statements
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    //JUnit Test for Update employee by id REST API -positive scenario
+    @Test
+    public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdatedEmployeeObject() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        Employee savedEmployee = Employee.builder()
+                .firstName("Devang")
+                .lastName("Chauhan")
+                .email("devang@gmail.com")
+                .build();
+
+        Employee updatedEmployee = Employee.builder()
+                .firstName("Jay")
+                .lastName("Parikh")
+                .email("jay@gmail.com")
+                .build();
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(savedEmployee));
+        given(employeeService.updateEmployee(any(Employee.class))).willAnswer(invocation -> invocation.getArgument(0));
+
+        //when - action or behaviour we are going to test
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //then - verify the resul or output using assert statements
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
+    }
+
+    //JUnit Test for Update employee by id REST API -negative scenario
+    @Test
+    public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnStatus404() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        Employee savedEmployee = Employee.builder()
+                .firstName("Devang")
+                .lastName("Chauhan")
+                .email("devang@gmail.com")
+                .build();
+
+        Employee updatedEmployee = Employee.builder()
+                .firstName("Jay")
+                .lastName("Parikh")
+                .email("jay@gmail.com")
+                .build();
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+
+        //when - action or behaviour we are going to test
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //then - verify the resul or output using assert statements
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    //JUnit Test for delete employee REST API
+    @Test
+    public void givenEmployeeId_whenDeleteEmployee_thenReturnStatus200() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        willDoNothing().given(employeeService).deleteEmployee(employeeId);
+
+        //when - action or behaviour we are going to test
+        ResultActions response = mockMvc.perform(delete("/api/employees/{id}", employeeId));
+
+        //then - verify the resul or output using assert statements
+        response.andExpect(status().isOk())
+                .andDo(print());
     }
 
 }
